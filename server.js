f6e1fc0164ea1f9405c6fdf2b69919dcdd3dda4b
Tyraine <danogaryjardine@gmail.com>
2022-04-16 21:39:23 -0600

    Starter code for my project

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/memory-haven');
+
+app.use(require('/controllers'));
+
+app.listen(PORT, () => console.log(`Connected to localhost on ${PORT}`));
